fbca69152852ec54135f9f9c9224b312cc3bb253
Amy Dang <kirontoo@gmail.com>
2023-03-07 16:36:55 -0800

    feat: build homepage
    - added the navbar
    - added a simple footer
    - fetched popular movies from tmdb api and rendered movie cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,120 +1,79 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import { Footer, Navbar, MovieImageCard } from "@/components";
+import { useState, useEffect } from "react";
+import { useDisclosure } from "@mantine/hooks";
+import { Container, Grid, LoadingOverlay } from "@mantine/core";
+import { TMDB_API_BASE_URL, TMDB_IMAGE_API_BASE_URL } from "@/lib";
+import { Movie } from '@/lib/types';
 
 export default function Home() {
+  const [visible, handlers] = useDisclosure(false);
+  const [data, setData] = useState<Movie[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    handlers.open();
+    const url = `${TMDB_API_BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.results);
+        setLoading(false);
+        handlers.close();
+      });
+  }, []);
+
+  const links = [
+    {
+      link: "/about",
+      label: "Features",
+    },
+    {
+      link: "/pricing",
+      label: "Pricing",
+    },
+    {
+      link: "/learn",
+      label: "Learn",
+    },
+    {
+      link: "/community",
+      label: "Community",
+    },
+  ];
+
   return (
     <>
       <Head>
         <title>FilmDB | Home</title>
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <Navbar links={links} />
+      <main>
+        {isLoading ? (
+          <LoadingOverlay visible={visible} overlayBlur={2} />
+        ) : (
+          <Container size="xl">
+            <Grid grow={false} columns={4}>
+              {data.map((m) => {
+                return (
+                  <Grid.Col sm={2} lg={1} key={m.id}>
+                    <MovieImageCard
+                      image={`${TMDB_IMAGE_API_BASE_URL}/w500/${m.poster_path}`}
+                      title={m.title}
+                      releaseDate={m.release_date}
+                      rating={m.vote_average}
+                    />
+                  </Grid.Col>
+                );
+              })}
+            </Grid>
+          </Container>
+        )}
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
