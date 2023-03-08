1961f2d8a9d1c27809f6ef324f21950776cd50ef
Amy Dang <kirontoo@gmail.com>
2023-03-08 15:34:12 -0800

    feat: add main layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import { Footer, Navbar } from "@/components";
+import { ReactNode } from "react";
+import { NavLinkProp } from "./Navbar";
+
+interface LayoutProps {
+  children: ReactNode;
+  links: NavLinkProp[]
+}
+
+export default function Layout({ links, children }: LayoutProps) {
+  return (
+    <>
+      <Navbar links={links} />
+      <main>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+}
