a08be652b82729d446d6cce3ce852b6896a599b6
Amy Dang <kirontoo@gmail.com>
2023-03-10 17:31:00 -0800

    refactor: fetch data before rendering

--- src/pages/media/[...slug].tsx
+++ src/pages/media/[...slug].tsx
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router";
 import { Button, createStyles, Container, LoadingOverlay } from "@mantine/core";
-import useSwr from "swr";
 import { buildTMDBQuery } from "@/lib/tmdb";
 import { Media as MediaType } from "@/lib/types";
+import { useMediaContext } from "@/context/MediaProvider";
+import Head from "next/head";
+import { NextPage } from 'next'
 
 const useStyles = createStyles((theme) => ({
   addBtn: {
@@ -10,32 +12,51 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-export default function Media() {
+interface MediaProps {
+  media: MediaType;
+}
+
+const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
   const router = useRouter();
   const slug = (router.query.slug as string[]) || [];
   const { classes } = useStyles();
+  const { addToWatchedMedia } = useMediaContext();
 
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const addToWatchedList = (media: MediaType) => {
+    media.media_type = slug[0];
+    addToWatchedMedia(media);
+  };
 
-  const { data, error, isLoading, mutate } = useSwr<MediaType>(
-    buildTMDBQuery(`${slug[0]}/${slug[1]}`),
-    fetcher,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
   return (
-    <Container size="xl">
-      {isLoading && data !== undefined ? (
-        <LoadingOverlay overlayBlur={2} visible={true} />
-      ) : (
+    <>
+      <Head>
+        <title>FilmDB | {media?.name ?? media?.title}</title>
+      </Head>
+      <Container size="xl">
         <>
-          <div>id: {slug[1]}, name: {data.name}</div>
-          <Button className={classes.addBtn}>Add to watched list</Button>
+          <div>
+            id: {slug[1]}, name: {media?.name ?? media?.title}
+          </div>
+          <Button
+            className={classes.addBtn}
+            onClick={() => addToWatchedList(media)}
+          >
+            Add to watched list
+          </Button>
         </>
-      )}
-    </Container>
+      </Container>
+    </>
   );
 }
+
+Media.getInitialProps = async (ctx) => {
+  const slug = ctx.query.slug;
+  const url = buildTMDBQuery(`${slug![0]}/${slug![1]}`);
+  const res = await fetch(url);
+  const data = await res.json();
+  data.media_type = slug![0];
+
+  return { media: data };
+};
+
+export default Media;
