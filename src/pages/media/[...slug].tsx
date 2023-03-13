1de471e2479e26b012c4b68714606dddd0e2132c
Amy Dang <kirontoo@gmail.com>
2023-03-13 14:24:27 -0700

    feat: render media info and poster

--- src/pages/media/[...slug].tsx
+++ src/pages/media/[...slug].tsx
@@ -1,14 +1,28 @@
 import { useRouter } from "next/router";
-import { Button, createStyles, Container, LoadingOverlay } from "@mantine/core";
-import { buildTMDBQuery } from "@/lib/tmdb";
+import {
+  Stack,
+  Text,
+  Button,
+  createStyles,
+  Image,
+  Container,
+  Flex,
+} from "@mantine/core";
+import { buildTMDBImageURL, buildTMDBQuery } from "@/lib/tmdb";
 import { Media as MediaType } from "@/lib/types";
 import { useMediaContext } from "@/context/MediaProvider";
 import Head from "next/head";
-import { NextPage } from 'next'
+import { NextPage } from "next";
 
 const useStyles = createStyles((theme) => ({
   addBtn: {
     textTransform: "uppercase",
+    "@media (min-width: 62em)": {
+      width: "fit-content",
+    },
+  },
+  infoContainer: {
+    width: "100%",
   },
 }));
 
@@ -21,7 +35,6 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
   const slug = (router.query.slug as string[]) || [];
   const { classes } = useStyles();
   const { addToWatchedMedia } = useMediaContext();
-
   const addToWatchedList = (media: MediaType) => {
     media.media_type = slug[0];
     addToWatchedMedia(media);
@@ -34,20 +47,39 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
       </Head>
       <Container size="xl">
         <>
-          <div>
-            id: {slug[1]}, name: {media?.name ?? media?.title}
-          </div>
-          <Button
-            className={classes.addBtn}
-            onClick={() => addToWatchedList(media)}
+          <Flex
+            gap="md"
+            justify="flex-start"
+            align="flex-start"
+            direction={{ base: "column", lg: "row" }}
           >
-            Add to watched list
-          </Button>
+            <Container>
+              <Image
+                src={buildTMDBImageURL(media?.poster_path, 500)}
+                alt={`${media?.title ?? media?.name} poster`}
+                radius="md"
+              />
+            </Container>
+
+            <Stack spacing="sm">
+              <Text component="h1">{media?.title ?? media?.name}</Text>
+              <Text component="h3">
+                {media?.release_date ?? "unknown release date"}
+              </Text>
+              <Text component="p">{media?.overview}</Text>
+              <Button
+                className={classes.addBtn}
+                onClick={() => addToWatchedList(media)}
+              >
+                Add to watched list
+              </Button>
+            </Stack>
+          </Flex>
         </>
       </Container>
     </>
   );
-}
+};
 
 Media.getInitialProps = async (ctx) => {
   const slug = ctx.query.slug;
