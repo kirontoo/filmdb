513e6c7ba8ec5935835532d8efc60c7da9d94f04
Amy Dang <kirontoo@gmail.com>
2023-03-13 13:39:15 -0700

    feat: helper for building tmdb image urls

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,9 +1,19 @@
-export const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
-export const TMDB_IMAGE_API_BASE_URL = 'https://image.tmdb.org/t/p'
+export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_IMAGE_API_BASE_URL = "https://image.tmdb.org/t/p";
 
 // NOTE: query assumes that it is already in URI encoded format
 // TODO: URI encode query for valid url()s
 export function buildTMDBQuery(path: string, query: string | null = null) {
-  return `${TMDB_API_BASE_URL}/${path}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY
-    }${query ? `&${query}` : ""}`;
+  return `${TMDB_API_BASE_URL}/${path}?api_key=${
+    process.env.NEXT_PUBLIC_TMDB_API_KEY
+  }${query ? `&${query}` : ""}`;
+}
+
+export function buildTMDBImageURL(
+  path: string | null,
+  size: 342 | 500 = 342
+): string {
+  return path
+    ? `${TMDB_IMAGE_API_BASE_URL}/w${size}/${path}`
+    : "https://placeholder.pics/svg/350x500";
 }
