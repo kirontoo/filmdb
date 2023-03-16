642a3d686bb033854a12e163f4538c83368c1fe3
Amy Dang <kirontoo@gmail.com>
2023-03-16 16:15:02 -0700

    chore: add logging

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -4,7 +4,28 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const client =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export default client;
