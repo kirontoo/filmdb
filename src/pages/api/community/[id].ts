87a724ec0d35bfe96829b1fef533c87735d32e4c
Amy Dang <kirontoo@gmail.com>
2023-03-29 14:06:08 -0700

    feat: endpoint to update community info

--- src/pages/api/community/[id].ts
+++ src/pages/api/community/[id].ts
@@ -13,29 +13,28 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const session = await getServerSession(req, res, authOptions);
+  const { query, method, body } = req;
+  const { id } = query;
+  if (!id) {
+    return res.status(400).send({
+      status: "fail",
+      message: "missing id",
+    });
+  }
+
+  if (!body) {
+    return res.status(400).send({
+      status: "fail",
+      message: "missing data",
+    });
+  }
 
   if (session) {
     // Signed in
-    const { query, method, body } = req;
 
     switch (method) {
       case "POST":
         // query: api/community/[id]
-
-        const { id } = query;
-        if (!id) {
-          return res.status(400).send({
-            status: "fail",
-            message: "missing id",
-          });
-        }
-
-        if (!body) {
-          return res.status(400).send({
-            status: "fail",
-            message: "missing data",
-          });
-        }
 
         try {
           const communityId: string = Array.isArray(id) ? id[0] : id;
@@ -120,17 +119,91 @@ export default async function handler(
             message: "could not add media to community",
           });
         }
+      case "PATCH":
+          const communityId: string = Array.isArray(id) ? id[0] : id;
+        await updateCommunity(req, res, communityId, session!.user!.email as string);
       default:
-        res.setHeader("Allow", ["POST"]);
+        res.setHeader("Allow", ["POST", "PATCH"]);
         return res.status(405).end(`Method ${method} Not Allowed`);
     }
   } else {
     // Not Signed in
     res.status(401).send({
       status: "error",
-      message: "user must be logged in",
+      message: "not authenticated",
     });
   }
 
   res.end();
 }
+
+const updateCommunity = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  communityId: string,
+  email: string
+) => {
+  const { body } = req;
+  try {
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        email: email,
+        communities: {
+          some: { id: communityId },
+        },
+      },
+      include: {
+        communities: {
+          where: {
+            id: communityId,
+          },
+          select: { id: true, name: true, description: true, createdBy: true },
+        },
+      },
+    });
+
+    if (user && user.communities[0].createdBy == user.id) {
+      const { name, description } = user.communities[0];
+      const updated = await prisma.community.update({
+        where: {
+          id: communityId,
+        },
+        data: {
+          name: body["name"] ?? name,
+          description: body["description"] ?? description ?? "",
+        },
+      });
+
+      if (updated) {
+        return res.status(200).json({
+          status: "success",
+          data: { community: updated },
+        });
+      }
+    } else {
+      return res.status(400).send({
+        status: "error",
+        message: "unauthorized",
+      });
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      return res.status(400).send({
+        status: "fail",
+        message: e.message,
+      });
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      return res.status(400).send({
+        status: "fail",
+        message: "missing data",
+      });
+    }
+
+    return res.status(500).send({
+      status: "error",
+      message: "",
+    });
+  }
+};
