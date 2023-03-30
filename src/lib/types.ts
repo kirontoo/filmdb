5b3aab80a9f5cfb9534eeb493f89d1c8a7b3c21d
Amy Dang <kirontoo@gmail.com>
2023-03-30 12:28:03 -0700

    chore: TMDB API response types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -18,3 +18,125 @@ export interface Media {
   media_type: string;
 }
 
+export type TMDBMedia = TMDBTvShow & TMDBMovie;
+
+export interface TMDBTvShow {
+  backdrop_path: string | null;
+  created_by: Array<{
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string | null;
+  }>;
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path: string | null;
+    vote_average: number;
+    vote_count: number;
+  };
+  name: string;
+  next_episode_to_air: null;
+  networks: {
+    name: string;
+    id: number;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: Array<{
+    id: number;
+    logo_path: null | string;
+    name: string;
+    origin_country: string;
+  }>;
+  production_countries: Array<{
+    iso_3166_1: string;
+    name: string;
+  }>;
+  seasons: Array<{
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+  }>;
+  spoken_languages: Array<{
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }>;
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface TMDBMovie {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: null | object;
+  budget: number;
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  original_language: string;
+  original_title: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: Array<{
+    name: string;
+    id: number;
+    logo_path: string | null;
+    origin_country: string;
+  }>;
+  production_countries: Array<{
+    iso_3166_1: string;
+    name: string;
+  }>;
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: Array<{
+    iso_639_1: string;
+    name: string;
+  }>;
+  status: string;
+  tagline: string | null;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
