1c20e715a4838b961dbf172ddc8adf8b7593d632
Amy Dang <kirontoo@gmail.com>
2023-04-05 17:32:26 -0700

    feat: show user logged in with avatar

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -1,7 +1,48 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { createStyles, Button } from "@mantine/core";
+import {
+  createStyles,
+  Button,
+  Container,
+  Avatar,
+  UnstyledButton,
+  Group,
+  Text,
+  Menu,
+  rem,
+} from "@mantine/core";
+import {
+  IconLogout,
+  IconHeart,
+  IconStar,
+  IconMessage,
+  IconSettings,
+  IconPlayerPause,
+  IconTrash,
+  IconSwitchHorizontal,
+  IconChevronDown,
+} from "@tabler/icons-react";
+import { useState } from "react";
 
 const useStyles = createStyles((theme) => ({
+  user: {
+    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    transition: "background-color 100ms ease",
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+    },
+
+    [theme.fn.smallerThan("xs")]: {
+      display: "none",
+    },
+  },
+  userActive: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+  },
   button: {
     [theme.fn.smallerThan("xs")]: {
       display: "none",
@@ -11,16 +52,49 @@ const useStyles = createStyles((theme) => ({
 
 export default function LoginBtn() {
   const { data: session } = useSession();
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
+
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
   if (session) {
     return (
       <>
-        <span className={classes.button}>
-          Signed in as {session.user?.email}
-        </span>
-        <Button className={classes.button} onClick={() => signOut()}>
-          Sign out
-        </Button>
+        <Menu
+          width={260}
+          position="bottom-end"
+          transitionProps={{ transition: "pop-top-right" }}
+          onClose={() => setUserMenuOpened(false)}
+          onOpen={() => setUserMenuOpened(true)}
+        >
+          <Menu.Target>
+            <UnstyledButton
+              className={cx(classes.user, {
+                [classes.userActive]: userMenuOpened,
+              })}
+            >
+              <Group spacing={7}>
+                <Avatar
+                  src={session.user?.image}
+                  alt={session.user?.name ?? ""}
+                  radius="xl"
+                  size={30}
+                />
+                <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
+                  {session.user?.name}
+                </Text>
+                <IconChevronDown size={rem(12)} stroke={1.5} />
+              </Group>
+            </UnstyledButton>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item
+              icon={<IconLogout size="0.9rem" stroke={1.5} />}
+              onClick={() => signOut()}
+            >
+              Logout
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </>
     );
   }
