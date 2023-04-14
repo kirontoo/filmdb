42256437b114319c513fecd93a6cd230b530cdcc
Amy Dang <kirontoo@gmail.com>
2023-04-14 16:34:50 -0700

    refactor

--- src/pages/api/community/[communityId]/media/[mediaId].ts
+++ src/pages/api/community/[communityId]/media/[mediaId].ts
@@ -7,73 +7,125 @@ import {
   PrismaClientKnownRequestError,
   PrismaClientValidationError,
 } from "@prisma/client/runtime/library";
+import {
+  APIError,
+  QueryError,
+  UnauthorizedError,
+  ValidationError,
+} from "@/lib/errors";
+import { apiHandler } from "@/lib/apiHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await getServerSession(req, res, authOptions);
-  const { query, method } = req;
-  const { mediaId } = query;
+export default apiHandler({
+  delete: deleteMedia,
+  patch: updateMedia,
+});
 
-  if (!mediaId) {
-    return res.status(400).send({
-      status: "fail",
-      message: "missing id",
-    });
-  }
-
-  if (session) {
-    // Signed in
-
-    switch (method) {
-      case "DELETE":
-        const mId: string = Array.isArray(mediaId) ? mediaId[0] : mediaId;
-        await deleteMedia(req, res, mId);
-
-      default:
-        res.setHeader("Allow", ["DELETE"]);
-        return res.status(405).end(`Method ${method} Not Allowed`);
-    }
-  } else {
-    // Not Signed in
-    res.status(401).send({
-      status: "error",
-      message: "not authenticated",
-    });
-  }
-
-  res.end();
-}
-
-async function deleteMedia(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  id: string
-) {
+async function deleteMedia(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const media = await prisma.media.delete({
-      where: { id: id },
+    const session = await getServerSession(req, res, authOptions);
+    const { query } = req;
+    const { mediaId } = query;
+    const id: string = Array.isArray(mediaId) ? mediaId[0] : mediaId!;
+
+    // check permission
+    const community = await prisma.community.findFirstOrThrow({
+      where: {
+        members: { some: { id: session!.user!.id } },
+      },
     });
-    if (media) {
-      return res.status(200).json({
-        status: "success",
-        data: {
-          media,
-        },
+
+    if (community) {
+      const media = await prisma.media.delete({
+        where: { id: id },
       });
+
+      if (media) {
+        return res.status(200).json({
+          status: "success",
+          data: {
+            media,
+          },
+        });
+      }
     }
   } catch (e) {
     if (e instanceof PrismaClientKnownRequestError) {
-      return res.status(400).send({
-        status: "fail",
-        message: e.message,
-      });
+      // Record Not Found
+      if (e.code == "P2015") {
+        throw new APIError("could not delete media");
+      }
+
+      throw new APIError(e.message, QueryError);
     }
 
-    return res.status(500).send({
-      status: "error",
-      message: "could not delete media",
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+
+    throw e;
+  }
+}
+
+async function updateMedia(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { query, body } = req;
+    const { mediaId, communityId } = query;
+    const id: string = Array.isArray(mediaId) ? mediaId[0] : mediaId!;
+    const cId: string = Array.isArray(communityId)
+      ? communityId[0]
+      : communityId!;
+    const session = await getServerSession(req, res, authOptions);
+
+    // make sure user has permission
+    // user is a member of the community
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        email: session!.user!.email,
+        communities: {
+          some: {
+            id: cId,
+            members: {
+              some: { id: session!.user!.id },
+            },
+          },
+        },
+      },
     });
+
+    if (user) {
+      const watchedPropExists = body.hasOwnProperty("watched");
+      const media = await prisma.media.update({
+        where: {
+          id: id,
+        },
+        data: {
+          watched: body.watched,
+          dateWatched: watchedPropExists ? new Date() : undefined,
+        },
+      });
+
+      if (media) {
+        return res.status(200).json({
+          status: "success",
+          data: {
+            media,
+          },
+        });
+      } else {
+        throw new Error("could not update media");
+      }
+    } else {
+      throw new APIError("not authenticated", UnauthorizedError);
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+
+    throw e;
   }
 }
