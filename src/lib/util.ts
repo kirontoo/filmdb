ad2a6a0d9f3962bc37dacadf078e43f16a9c62f7
Amy Dang <kirontoo@gmail.com>
2023-04-17 17:27:04 -0700

    refactor: move to /api/community/communityId/media/id

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -17,15 +17,18 @@ export async function updateMedia(media: Media): Promise<{
   res: Response;
   data: { status: string; data: { media: Media } };
 }> {
-  const res = await fetch(`/api/media/${media.id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      watched: !media.watched,
-    }),
-  });
+  const res = await fetch(
+    `/api/community/${media.communityId}/media/${media.id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        watched: !media.watched,
+      }),
+    }
+  );
 
   const data = await res.json();
   return { res, data };
