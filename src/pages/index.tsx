fec0ec426d824f95637e6b120a712df07a215a80
Amy Dang <kirontoo@gmail.com>
2023-04-24 17:21:21 -0700

    feat: rework home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+
+import useSwr from "swr";
+import format from "date-format";
 import {
   MediaImageCard,
   MediaImageCardHeader,
@@ -7,6 +10,7 @@ import {
 import { useState, useEffect } from "react";
 import { useDisclosure } from "@mantine/hooks";
 import {
+  Image,
   rem,
   createStyles,
   Text,
@@ -14,12 +18,26 @@ import {
   Grid,
   Title,
   LoadingOverlay,
+  BackgroundImage,
+  Stack,
+  Flex,
+  Group,
+  Code,
+  Button,
+  Overlay,
 } from "@mantine/core";
-import { IconStarFilled } from "@tabler/icons-react";
-import { TMDB_IMAGE_API_BASE_URL } from "@/lib/tmdb";
+import { IconPlus, IconStarFilled } from "@tabler/icons-react";
+import {
+  buildTMDBImageURL,
+  getTMDBBackdropImageSrcSet,
+  getTMDBPosterImageSrcSet,
+  TMDB_IMAGE_API_BASE_URL,
+} from "@/lib/tmdb";
 import { TMDBMedia } from "@/lib/types";
 import { buildTMDBQuery } from "@/lib/tmdb";
 import Link from "next/link";
+import useIsDesktopDevice from "@/lib/hooks/useIsDesktopDevice";
+import { Carousel } from "@mantine/carousel";
 
 const useStyles = createStyles((theme) => ({
   title: {
@@ -48,69 +66,160 @@ const useStyles = createStyles((theme) => ({
     alignItems: "center",
     gap: rem(5),
   },
+
+  showcaseImage: {
+    height: "65vh",
+    [`@media(min-width:${theme.breakpoints.md})`]: {
+      height: "45vh",
+    },
+  },
+
+  showcaseContent: {
+    color: theme.white,
+    padding: theme.spacing.md,
+  },
+
+  visibleContent: {
+    zIndex: 300,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    position: "relative",
+  },
+
+  trendingList: {
+    [`@media(max-width:${theme.breakpoints.md})`]: {
+      marginBottom: "7rem",
+    },
+  },
 }));
 
 export default function Home() {
-  const [visible, handlers] = useDisclosure(false);
-  const { classes } = useStyles();
-  const [data, setData] = useState<TMDBMedia[]>([]);
-  const [isLoading, setLoading] = useState(false);
+  const [visible] = useDisclosure(false);
+  const { classes, cx } = useStyles();
+  const [showcasedMedia, setShowcasedMedia] = useState<TMDBMedia | null>(null);
+  const isDesktop = useIsDesktopDevice();
 
-  useEffect(() => {
-    if (!data.length) {
-      setLoading(true);
-      handlers.open();
-      const url = buildTMDBQuery("movie/popular");
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data.results);
-          setLoading(false);
-          handlers.close();
-        });
-    }
-  }, []);
+  const fetcher = (url: string) =>
+    fetch(url)
+      .then((r) => r.json())
+      .then((d) => {
+        setShowcasedMedia(d.results[0]);
+        return d.results.slice(1);
+      });
+  const { data, isLoading } = useSwr<TMDBMedia[]>(
+    buildTMDBQuery("trending/all/week"),
+    fetcher
+  );
+
+  const trendingMediaCarousel = (mediaType: "tv" | "movie") => {
+    return (
+      data && (
+        <Carousel
+          align="start"
+          controlsOffset="xs"
+          loop
+          withControls={isDesktop}
+          breakpoints={[
+            {
+              minWidth: "md",
+              slideSize: "28%",
+              slideGap: "md",
+            },
+            {
+              maxWidth: "md",
+              slideSize: "33.33%",
+              slideGap: "xs",
+            },
+          ]}
+        >
+          {data
+            .filter((m) => m.media_type == mediaType)
+            .map((m) => (
+              <Carousel.Slide>
+                <Link href={`/media/${mediaType}/${m.id}`}>
+                  <Image
+                    radius="sm"
+                    src={`${TMDB_IMAGE_API_BASE_URL}/w${isDesktop ? "342" : "185"
+                      }/${m.poster_path}`}
+                  />
+                </Link>
+              </Carousel.Slide>
+            ))}
+        </Carousel>
+      )
+    );
+  };
 
   return (
     <>
       <Head>
-        <title>FilmDB | Home</title>
+        <title>Home | FilmDB</title>
       </Head>
       {isLoading ? (
         <LoadingOverlay visible={visible} overlayBlur={2} />
       ) : (
-        <Container size="xl">
-          <Grid grow={false} columns={4}>
-            {data.map((m) => {
-              return (
-                <Grid.Col sm={2} lg={1} key={m.id}>
-                  <MediaImageCard
-                    component={Link}
-                    href={`/media/movie/${m.id}`}
-                    image={`${TMDB_IMAGE_API_BASE_URL}/w500/${m.poster_path}`}
-                  >
-                    <MediaImageCardHeader>
-                      <>
-                        <Text className={classes.date} size="xs">
-                          {m.release_date ?? m.first_air_date}
-                        </Text>
-                        <Title order={3} className={classes.title}>
-                          {m.title ?? m.name ?? m.original_title}
-                        </Title>
-                      </>
-                    </MediaImageCardHeader>
-                    <MediaImageCardFooter className={classes.rating}>
-                      <IconStarFilled
-                        style={{ position: "relative", color: "yellow" }}
-                      />
-                      <Text>{m.vote_average}</Text>
-                    </MediaImageCardFooter>
-                  </MediaImageCard>
-                </Grid.Col>
-              );
-            })}
-          </Grid>
-        </Container>
+        data &&
+        showcasedMedia && (
+          <>
+            <BackgroundImage
+              className={classes.showcaseImage}
+              src={`${TMDB_IMAGE_API_BASE_URL}/${isDesktop ? "w1280" : "w342"
+                }/${isDesktop
+                  ? showcasedMedia!.backdrop_path
+                  : showcasedMedia!.poster_path
+                }`}
+            >
+              <Overlay gradient="linear-gradient(180deg, rgba(0,0,0,0.79),  rgba(0,0,0,0) 20%,  rgba(0,0,0,0.2) 55%, rgba(0,0,0,1) 65%, rgba(0,0,0,0) 0)" />
+              <Container
+                h="100%"
+                sx={{ zIndex: 300, position: "relative" }}
+                className={cx(classes.visibleContent, classes.showcaseContent)}
+              >
+                <Stack spacing="sm">
+                  <div>
+                    <Text>Trending Now:</Text>
+                    <Title order={1} fz="xl">
+                      {showcasedMedia!.title ??
+                        showcasedMedia!.name ??
+                        showcasedMedia!.original_name ??
+                        showcasedMedia!.original_title}
+                    </Title>
+                  </div>
+                  <Group>
+                    <Text>
+                      {format("yyyy", new Date(showcasedMedia!.release_date))}
+                    </Text>
+                    <Button size="sm" color="dark" compact radius="xl">
+                      Info
+                    </Button>
+                    <Button
+                      leftIcon={<IconPlus size={rem(16)} />}
+                      radius="xl"
+                      size="sm"
+                      compact
+                    >
+                      Add
+                    </Button>
+                  </Group>
+                </Stack>
+              </Container>
+            </BackgroundImage>
+            <Container
+              className={cx(classes.visibleContent, classes.trendingList)}
+            >
+              <Stack my={rem(32)}>
+                <Text fw="bold" tt="capitalize">
+                  trending movies
+                </Text>
+                {trendingMediaCarousel("movie")}
+
+                <Text fw="bold" tt="capitalize">
+                  trending tv shows
+                </Text>
+                {trendingMediaCarousel("tv")}
+              </Stack>
+            </Container>
+          </>
+        )
       )}
     </>
   );
