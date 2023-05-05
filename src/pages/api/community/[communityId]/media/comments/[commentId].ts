3387d150aee2d954552e45ffe7a6e6405011a9c7
Amy Dang <kirontoo@gmail.com>
2023-05-05 16:06:31 -0700

    feat: GET, POST, PATCH, DELETE comment endpoint

---
+++ src/pages/api/community/[communityId]/media/comments/[commentId].ts
@@ -0,0 +1,118 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/lib/prismadb";
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
+import {
+  APIError,
+  QueryError,
+  UnauthorizedError,
+  ValidationError,
+} from "@/lib/errors";
+
+import { apiHandler } from "@/lib/apiHandler";
+
+export default apiHandler({
+  patch: updateComment,
+  delete: deleteComment,
+});
+
+async function updateComment(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const { commentId } = req.query;
+    const cId: string = Array.isArray(commentId) ? commentId[0] : commentId!;
+    const { text } = req.body;
+
+    if (text === undefined || text === null) {
+      throw new APIError("missing text value", ValidationError);
+    }
+
+    const comment = await prisma.comment.update({
+      where: {
+        userId_id: {
+          id: cId,
+          userId: session!.user!.id,
+        },
+      },
+      data: {
+        text: text,
+      },
+    });
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        comment,
+      },
+    });
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+    throw e;
+  }
+}
+
+// ONLY THE OWNER OR THE COMMUNITY OWNER CAN DELETE
+async function deleteComment(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const { communityId, commentId } = req.query;
+    const cmId: string = Array.isArray(commentId) ? commentId[0] : commentId!;
+    const cId: string = Array.isArray(communityId)
+      ? communityId[0]
+      : communityId!;
+
+    const community = await prisma.community
+      .findFirst({
+        where: {
+          AND: [{ id: cId }, { members: { some: { id: session!.user!.id } } }],
+        },
+      })
+      .catch(() => {
+        throw new APIError("not authorized", UnauthorizedError);
+      });
+
+    if (community) {
+      const comment = await prisma.comment.delete({
+        where: {
+          userId_id: {
+            id: cmId,
+            userId: session!.user!.id,
+          },
+        },
+      });
+      return res.status(200).json({
+        status: "success",
+        data: {
+          comment,
+        },
+      });
+    } else {
+      throw new APIError("not authorized", UnauthorizedError);
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+    throw e;
+  }
+}
---
+++ src/pages/api/community/[communityId]/media/comments/index.ts
@@ -0,0 +1,111 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/lib/prismadb";
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
+import {
+  APIError,
+  QueryError,
+  UnauthorizedError,
+  ValidationError,
+} from "@/lib/errors";
+
+import { apiHandler } from "@/lib/apiHandler";
+
+export default apiHandler({
+  get: getComments,
+  post: createComment,
+});
+
+async function getComments(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const { mediaId } = req.query;
+    const mId: string = Array.isArray(mediaId) ? mediaId[0] : mediaId!;
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        mediaId: mId,
+      },
+    });
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        comments,
+      },
+    });
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+    throw e;
+  }
+}
+
+async function createComment(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const { communityId, mediaId } = req.query;
+    const mId: string = Array.isArray(mediaId) ? mediaId[0] : mediaId!;
+    const { text, parentId } = req.body;
+    const cId: string = Array.isArray(communityId)
+      ? communityId[0]
+      : communityId!;
+
+    if (text === undefined || text === null) {
+      throw new APIError("missing text value", ValidationError);
+    }
+
+    const community = await prisma.community
+      .findFirst({
+        where: {
+          AND: [{ id: cId }, { members: { some: { id: session!.user!.id } } }],
+        },
+      })
+      .catch(() => {
+        throw new APIError("not authorized", UnauthorizedError);
+      });
+
+    if (community) {
+      const comment = await prisma.comment.create({
+        data: {
+          user: { connect: { id: session!.user!.id } },
+          media: { connect: { id: mId } },
+          text: text,
+          parent: { connect: { id: parentId ? parentId : undefined } },
+        },
+      });
+
+      return res.status(201).json({
+        status: "success",
+        data: {
+          comment,
+        },
+      });
+    } else {
+      throw new APIError("not authorized", UnauthorizedError);
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+    throw e;
+  }
+}
