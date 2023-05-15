3f15d370a999d461936a14a0426696b762b8893c
Amy Dang <kirontoo@gmail.com>
2023-05-15 16:04:31 -0700

    feat: show a comment text input on reply

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,14 +1,37 @@
-import { createStyles, Text, Avatar, Group, rem } from "@mantine/core";
+import {
+  createStyles,
+  Text,
+  Avatar,
+  Group,
+  rem,
+  Button,
+  Spoiler,
+  TypographyStylesProvider,
+  Collapse,
+  Space,
+  Stack,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import CommentTextEditor from "./CommentTextEditor";
 
 const useStyles = createStyles((theme) => ({
   body: {
     paddingLeft: rem(54),
     paddingTop: theme.spacing.sm,
   },
+  actionBar: {
+    paddingLeft: rem(54),
+  },
+  content: {
+    fontSize: theme.fontSizes.md,
+    "& > p:last-child": {
+      marginBottom: 0,
+    },
+  },
 }));
 
 interface CommentProps {
-  createdAt: string;
+  createdAt: any;
   body: string;
   author: {
     name: string;
@@ -18,20 +41,47 @@ interface CommentProps {
 
 function Comment({ createdAt, body, author }: CommentProps) {
   const { classes } = useStyles();
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <div>
       <Group>
         <Avatar src={author.image} alt={author.name} radius="xl" />
         <div>
-          <Text size="sm">{author.name}</Text>
+          <Text size="md">{author.name}</Text>
           <Text size="xs" color="dimmed">
             {createdAt}
           </Text>
         </div>
       </Group>
-      <Text className={classes.body} size="sm">
-        {body}
-      </Text>
+      <Spoiler
+        className={classes.body}
+        maxHeight={100}
+        showLabel="Read more"
+        hideLabel="Show less"
+      >
+        <TypographyStylesProvider>
+          <div
+            className={classes.content}
+            dangerouslySetInnerHTML={{ __html: body }}
+          />
+        </TypographyStylesProvider>
+      </Spoiler>
+      <Group className={classes.body}>
+        <Button color="gray" compact variant="subtle" onClick={open}>
+          Reply
+        </Button>
+        <Collapse in={opened}>
+          <Stack>
+            <CommentTextEditor />
+            <Group position="right">
+              <Button variant="subtle" color="gray" onClick={close}>
+                Cancel
+              </Button>
+              <Button variant="light">Comment</Button>
+            </Group>
+          </Stack>
+        </Collapse>
+      </Group>
     </div>
   );
 }
