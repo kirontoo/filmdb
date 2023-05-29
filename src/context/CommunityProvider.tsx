87b2319844efc3bca49e65a954e3d978472d84ee
Amy Dang <kirontoo@gmail.com>
2023-05-29 15:00:03 -0700

    fix: isFetching should default to false

--- src/context/CommunityProvider.tsx
+++ src/context/CommunityProvider.tsx
@@ -62,7 +62,7 @@ export const useCommunityProvider = (): CommunityState => {
     useState<number>(-1);
   const { status } = useSession();
   const { setLoading } = useLoadingContext();
-  const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     setLoading(true);
