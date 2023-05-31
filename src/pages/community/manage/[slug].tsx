e94318cdd07bc43ddf7a0d69486100189430591a
Amy Dang <kirontoo@gmail.com>
2023-05-31 15:32:13 -0700

    feat: draggable ui for reordering media

---
+++ src/pages/community/manage/[slug].tsx
@@ -0,0 +1,160 @@
+import {
+  Container,
+  createStyles,
+  Text,
+  rem,
+  Button,
+  Group,
+} from "@mantine/core";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useListState } from "@mantine/hooks";
+import { Media } from "@prisma/client";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing.md,
+    paddingBottom: theme.spacing.md,
+  },
+  item: {
+    ...theme.fn.focusStyles(),
+    display: "flex",
+    alignItems: "center",
+    borderRadius: theme.radius.md,
+    border: `${rem(1)} solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+      }`,
+    padding: `${theme.spacing.sm} ${theme.spacing.xl}`,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.white,
+    marginBottom: theme.spacing.sm,
+  },
+
+  itemDragging: {
+    boxShadow: theme.shadows.sm,
+  },
+
+  position: {
+    fontSize: rem(30),
+    fontWeight: 700,
+    width: rem(60),
+  },
+}));
+
+function ManageCommunitySlug() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [savingQueue, setSavingQueue] = useState<boolean>(false);
+
+  const { classes, cx } = useStyles();
+  const [queuedMedia, handlers] = useListState<Media>([]);
+
+  useEffect(() => {
+    loadData();
+  }, [slug]);
+
+  async function loadData() {
+    setLoading(true);
+    try {
+      const communitySlug = Array.isArray(slug) ? slug[0] : slug;
+      if (communitySlug) {
+        const res = await fetch(`/api/community/${communitySlug}/media`);
+        if (res.ok) {
+          const { data } = await res.json();
+          const media = data.medias
+            .filter((m: Media) => !m.watched)
+            .map((m: Media, index: number) => ({ ...m, queue: index }));
+          handlers.setState(media);
+        } else {
+          throw new Error("community does not exist");
+        }
+      }
+    } catch (e) {
+      router.push("/404");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const saveQueue = async () => {
+    try {
+      setSavingQueue(true);
+
+    } catch (error) {
+
+    } finally {
+      setSavingQueue(false);
+    }
+  }
+
+  const items = queuedMedia.map((item: Media, index) => (
+    <Draggable key={item.id} index={index} draggableId={item.id}>
+      {(provided, snapshot) => (
+        <div
+          className={cx(classes.item, {
+            [classes.itemDragging]: snapshot.isDragging,
+          })}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Text className={classes.position}>{(item.queue || 0) + 1}</Text>
+          <div>
+            <Text>{item.title}</Text>
+          </div>
+        </div>
+      )}
+    </Draggable>
+  ));
+
+  function DndList({ children }: DndListProps) {
+    return (
+      <DragDropContext
+        onDragEnd={(data) => {
+          const { destination, source } = data;
+          handlers.reorder({ from: source.index, to: destination?.index || 0 });
+          handlers.setItemProp(source.index || 0, "queue", source.index);
+          handlers.setItemProp(
+            destination?.index || 0,
+            "queue",
+            destination?.index || 0
+          );
+        }}
+      >
+        <Droppable droppableId="dnd-list" direction="vertical">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {children}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+
+  return (
+    <Container className={classes.container}>
+      {!isLoading && (
+        <Container size="xs">
+          <DndList>{items}</DndList>
+
+          <Group position="right">
+            <Button loading={savingQueue} onClick={saveQueue}>Save Queue</Button>
+          </Group>
+        </Container>
+      )}
+    </Container>
+  );
+}
+
+ManageCommunitySlug.auth = {
+  unauthorized: "/auth/signin",
+};
+
+export default ManageCommunitySlug;
+
+interface DndListProps {
+  children: React.ReactNode;
+}
