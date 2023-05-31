f5a9e88eb5f5689256507a4c6986a65a3112291b
Amy Dang <kirontoo@gmail.com>
2023-05-31 15:31:09 -0700

    feat: allow media query using object id or slug

--- src/pages/api/community/[communityId]/media/index.ts
+++ src/pages/api/community/[communityId]/media/index.ts
@@ -14,14 +14,14 @@ import {
 } from "@/lib/errors";
 
 import { apiHandler } from "@/lib/apiHandler";
+import { ObjectId } from "bson";
 
 export default apiHandler({
   get: getMedias,
 });
 
-
-// query: /api/media?community={community slug}
-// query: /api/media?communityId={community id}
+// query: /api/comunity/:communityId/media
+// communityId can be the community id OR the slug
 async function getMedias(
   req: NextApiRequest,
   res: NextApiResponse
@@ -29,19 +29,21 @@ async function getMedias(
   try {
     const session = await getServerSession(req, res, authOptions);
     const { query } = req;
-    const community: string = Array.isArray(query.community)
-      ? query.community[0]
-      : query.community!;
-    const communityId: string = Array.isArray(query.communityId)
+    let communityId: string | null = Array.isArray(query.communityId)
       ? query.communityId[0]
       : query.communityId!;
 
+    let slug = null;
+
+    // if it's not a valid object id, then it's a slug
+    if (!ObjectId.isValid(communityId)) {
+      slug = communityId;
+      communityId = null;
+    }
+
     const c = await prisma.community.findFirst({
       where: {
-        OR: [
-          { id: communityId || undefined },
-          { slug: community || undefined },
-        ],
+        OR: [{ id: communityId || undefined }, { slug: slug || undefined }],
         members: {
           some: { id: session!.user!.id },
         },
