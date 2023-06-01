d5ea80794e519b4d201cb9bb64aacecf3e7b03ec
Amy Dang <kirontoo@gmail.com>
2023-06-01 14:53:39 -0700

    feat: update multiple medias data endpoint

--- src/pages/api/community/[communityId]/media/index.ts
+++ src/pages/api/community/[communityId]/media/index.ts
@@ -15,9 +15,11 @@ import {
 
 import { apiHandler } from "@/lib/apiHandler";
 import { ObjectId } from "bson";
+import { Media } from "@prisma/client";
 
 export default apiHandler({
   get: getMedias,
+  patch: updateMedias,
 });
 
 // query: /api/comunity/:communityId/media
@@ -53,6 +55,9 @@ async function getMedias(
           orderBy: {
             title: "asc",
           },
+          include: {
+            requestedBy: true,
+          },
         },
       },
     });
@@ -62,6 +67,79 @@ async function getMedias(
         status: "success",
         data: {
           medias: c.medias,
+        },
+      });
+    } else {
+      throw new APIError("not authorized", UnauthorizedError);
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      throw new APIError(e.message, QueryError);
+    }
+
+    if (e instanceof PrismaClientValidationError) {
+      throw new APIError(e.message, ValidationError);
+    }
+    throw e;
+  }
+}
+
+/*
+ * payload:
+ * required: id, queue
+ */
+// NOTE: maybe move this to it's own endpoint since it's only for the
+// queue order
+async function updateMedias(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const { query, body } = req;
+    let communityId: string | null = Array.isArray(query.communityId)
+      ? query.communityId[0]
+      : query.communityId!;
+
+    let slug = null;
+    const mediasToUpdate = body.medias;
+
+    if (!mediasToUpdate) {
+      throw new APIError("no data to update", QueryError);
+    }
+
+    // if it's not a valid object id, then it's a slug
+    if (!ObjectId.isValid(communityId)) {
+      slug = communityId;
+      communityId = null;
+    }
+
+    // only the community owner can update the media
+    const c = await prisma.community.findFirst({
+      where: {
+        OR: [{ id: communityId || undefined }, { slug: slug || undefined }],
+        createdBy: session!.user!.id,
+      },
+    });
+
+    // update all media data
+    if (c) {
+      const transaction = await prisma.$transaction(
+        mediasToUpdate.map((m: Media) => {
+          return prisma.media.update({
+            where: {
+              id: m.id,
+            },
+            data: {
+              queue: m.queue || undefined,
+            },
+          });
+        })
+      );
+      return res.status(200).json({
+        status: "success",
+        data: {
+          medias: transaction,
         },
       });
     } else {
