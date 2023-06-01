18f7b54a6b7d9a1b96ffee29d304b649bc52c7f2
Amy Dang <kirontoo@gmail.com>
2023-06-01 16:03:02 -0700

    feat: reorder media on queue list and save it

--- src/pages/community/manage/[slug].tsx
+++ src/pages/community/manage/[slug].tsx
@@ -9,8 +9,14 @@ import {
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useListState } from "@mantine/hooks";
-import { Media } from "@prisma/client";
+import { Media, User } from "@prisma/client";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import Notify from "@/lib/notify";
+import { useCommunityContext } from "@/context/CommunityProvider";
+
+type MediaWithRequester = {
+  requestedBy: User;
+} & Media;
 
 const useStyles = createStyles((theme) => ({
   container: {
@@ -22,8 +28,9 @@ const useStyles = createStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     borderRadius: theme.radius.md,
-    border: `${rem(1)} solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
-      }`,
+    border: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
     padding: `${theme.spacing.sm} ${theme.spacing.xl}`,
     backgroundColor:
       theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.white,
@@ -46,9 +53,15 @@ function ManageCommunitySlug() {
   const { slug } = router.query;
   const [isLoading, setLoading] = useState<boolean>(false);
   const [savingQueue, setSavingQueue] = useState<boolean>(false);
-
+  const { currentCommunity } = useCommunityContext();
   const { classes, cx } = useStyles();
-  const [queuedMedia, handlers] = useListState<Media>([]);
+  const [queuedMedia, handlers] = useListState<MediaWithRequester>([]);
+
+  useEffect(() => {
+    if (currentCommunity) {
+      router.push(`/community/manage/${currentCommunity.slug}`);
+    }
+  }, [currentCommunity]);
 
   useEffect(() => {
     loadData();
@@ -62,9 +75,7 @@ function ManageCommunitySlug() {
         const res = await fetch(`/api/community/${communitySlug}/media`);
         if (res.ok) {
           const { data } = await res.json();
-          const media = data.medias
-            .filter((m: Media) => !m.watched)
-            .map((m: Media, index: number) => ({ ...m, queue: index }));
+          const media = data.medias.filter((m: Media) => !m.watched);
           handlers.setState(media);
         } else {
           throw new Error("community does not exist");
@@ -80,15 +91,30 @@ function ManageCommunitySlug() {
   const saveQueue = async () => {
     try {
       setSavingQueue(true);
-
+      const mediaToSave = queuedMedia.map((m, index) => ({
+        id: m.id,
+        queue: index + 1,
+      }));
+      const communitySlug = Array.isArray(slug) ? slug[0] : slug;
+      const res = await fetch(`/api/community/${communitySlug}/media`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ medias: mediaToSave }),
+      });
+      if (!res.ok) {
+        // const { data } = await res.json();
+        throw new Error("could not update queue");
+      }
     } catch (error) {
-
+      Notify.error("could not update queue");
     } finally {
       setSavingQueue(false);
     }
-  }
+  };
 
-  const items = queuedMedia.map((item: Media, index) => (
+  const items = queuedMedia.map((item: MediaWithRequester, index) => (
     <Draggable key={item.id} index={index} draggableId={item.id}>
       {(provided, snapshot) => (
         <div
@@ -99,9 +125,10 @@ function ManageCommunitySlug() {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <Text className={classes.position}>{(item.queue || 0) + 1}</Text>
+          <Text className={classes.position}>{index + 1}</Text>
           <div>
             <Text>{item.title}</Text>
+            <Text>{item.requestedBy?.name ?? ""}</Text>
           </div>
         </div>
       )}
@@ -114,12 +141,6 @@ function ManageCommunitySlug() {
         onDragEnd={(data) => {
           const { destination, source } = data;
           handlers.reorder({ from: source.index, to: destination?.index || 0 });
-          handlers.setItemProp(source.index || 0, "queue", source.index);
-          handlers.setItemProp(
-            destination?.index || 0,
-            "queue",
-            destination?.index || 0
-          );
         }}
       >
         <Droppable droppableId="dnd-list" direction="vertical">
@@ -141,7 +162,9 @@ function ManageCommunitySlug() {
           <DndList>{items}</DndList>
 
           <Group position="right">
-            <Button loading={savingQueue} onClick={saveQueue}>Save Queue</Button>
+            <Button loading={savingQueue} onClick={saveQueue}>
+              Save Queue
+            </Button>
           </Group>
         </Container>
       )}
