f5376eafedd739799050798118dee1765175c934
Amy Dang <kirontoo@gmail.com>
2023-07-10 16:04:41 -0700

    feat: add media to watched list

--- src/pages/media/[...slug].tsx
+++ src/pages/media/[...slug].tsx
@@ -16,6 +16,11 @@ import {
   Divider,
   Menu,
   ActionIcon,
+  Modal,
+  UnstyledButton,
+  Avatar,
+  Loader,
+  Center,
 } from "@mantine/core";
 import Link from "next/link";
 import { buildTMDBQuery, getTMDBShowcaseImageUrl } from "@/lib/tmdb";
@@ -39,6 +44,8 @@ import {
   IconPointFilled,
 } from "@tabler/icons-react";
 import dayjs from "dayjs";
+import { useDisclosure } from "@mantine/hooks";
+import { useCommunityContext } from "@/context/CommunityProvider";
 
 const useStyles = createStyles((theme) => ({
   mediaContainer: {
@@ -82,6 +89,15 @@ const useStyles = createStyles((theme) => ({
       backgroundImage:
         "radial-gradient(circle, rgba(0,0,0,0) 31%, rgba(0,0,0,0.6) 42%, rgba(0,0,0,1) 48%)",
     },
+  },
+
+  communityBtn: {
+    border: `1px solid ${theme.colors.gray[7]}`,
+    borderRadius: theme.radius.sm,
+    padding: theme.spacing.sm,
+    display: "flex",
+    alignItems: "center",
+    gap: "1rem",
   },
 
   menuDropdown: {
@@ -131,19 +147,20 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
   );
   const { classes } = useStyles();
   const { status } = useSession();
-  const [loadingQueueBtn, setLoadingQueueBtn] = useState<boolean>(false);
-  const [loadingWatchedBtn, setLoadingWatchedBtn] = useState<boolean>(false);
+  const [loadingAddToList, setLoadingAddToList] = useState<boolean>(false);
+  const { communities } = useCommunityContext();
   const isDesktop = useIsDesktopDevice();
   const isMovie = media.media_type === "movie";
   const certification = getCertifications(isMovie);
+  const [openedCommunityModal, communityModalControls] = useDisclosure(false);
 
   function getCertifications(isMovie: boolean) {
     if (media) {
       let cert = isMovie
         ? media.release_dates!.results.find((d) => d.iso_3166_1 == "US")
-            ?.release_dates[0].certification
+          ?.release_dates[0].certification
         : media.content_ratings!.results.find((d) => d.iso_3166_1 == "US")
-            ?.rating;
+          ?.rating;
       if (cert && cert !== "") {
         return cert;
       }
@@ -156,13 +173,8 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
     community: CommunityMenuActionProps,
     watched: boolean
   ) => {
-    if (watched) {
-      setLoadingWatchedBtn(true);
-    } else {
-      setLoadingQueueBtn(true);
-    }
-
     try {
+      setLoadingAddToList(true);
       const body = {
         ...media,
         tmdbId: media.id,
@@ -180,31 +192,25 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
           "Content-Type": "application/json",
         },
       });
-      const { message } = await res.json();
 
       if (res.ok) {
         Notify.success(
           `${community.name}`,
-          `${body.title} was added to ${
-            watched ? "watched list" : "queued list"
+          `${body.title} was added to ${watched ? "watched list" : "queued list"
           }`
         );
       } else {
-        Notify.error(`${community.name}`, `${message}`);
+        throw new Error("could not add to list");
       }
     } catch (e) {
       Notify.error(
-        `${
-          media.title ?? media.name ?? media.original_title
+        `${media.title ?? media.name ?? media.original_title
         } could not be added to ${community.name}`,
         "Please try again"
       );
     } finally {
-      if (watched) {
-        setLoadingWatchedBtn(false);
-      } else {
-        setLoadingQueueBtn(false);
-      }
+      setLoadingAddToList(true);
+      communityModalControls.close();
     }
   };
 
@@ -218,100 +224,131 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
         <title>{`${media.name ?? media.title}`} | FilmDB </title>
       </Head>
       <>
-        <>
-          <div className={classes.imgContainer}>
-            <Container>
-              <Image
-                src={getTMDBShowcaseImageUrl(
-                  isDesktop
-                    ? (media.backdrop_path as string)
-                    : (media.poster_path as string),
-                  isDesktop
-                )}
-                alt={`${media.title ?? media.name} poster`}
-              />
-              <div className={classes.overlay} />
-            </Container>
-          </div>
-          <Container className={classes.mediaContainer}>
-            <Stack spacing="sm" p="1rem">
-              <Group position="apart">
-                <Title order={1}>{media.title ?? media.name}</Title>
-                <Group>
-                  <AddMediaButton
-                    media={media}
-                    menuProps={{ position: "top-end" }}
-                  />
-
-                  {status == "authenticated" && (
-                    <Menu position="top-end">
-                      <Menu.Target>
-                        <ActionIcon variant="outline" radius="xl">
-                          <IconDotsVertical />
-                        </ActionIcon>
-                      </Menu.Target>
-                      <Menu.Dropdown className={classes.menuDropdown}>
-                        <Menu.Item icon={<IconHistory />}>
-                          Add to watched list
-                        </Menu.Item>
-                      </Menu.Dropdown>
-                    </Menu>
-                  )}
-                </Group>
-              </Group>
-              <Group spacing="sm">
-                <Badge
-                  variant="outline"
-                  radius="xs"
-                  className={classes.certification}
-                >
-                  {certification}
-                </Badge>
-                <IconPointFilled size="0.6rem" role="separator" />
-                <Text>
-                  {media.release_date || media.first_air_date
-                    ? dayjs(media.release_date ?? media.first_air_date).format(
-                        "YYYY"
-                      )
-                    : "Release Date: N/A"}
-                </Text>
-                {isMovie && (
-                  <>
-                    <IconPointFilled size="0.6rem" role="separator" />
-                    <Text>{formatDuration(media.runtime!)}</Text>
-                  </>
-                )}
-              </Group>
-
-              <Text>{media.genres.map((g) => g.name).join(", ")}</Text>
-              <Space h="xs" />
-
-              <Title order={2} fz="lg">
-                Overview
-              </Title>
-              <Text component="p">{media.overview}</Text>
-
-              <Divider />
-
-              <Title order={2} fz="lg">
-                Cast
-              </Title>
-
-              <ul className={styles.inlineList}>
-                {media.credits.cast.map((p) => {
-                  return <li key={p.id}>{p.name}</li>;
-                })}
-              </ul>
-
-              <Divider />
-
-              <Title order={2} fz="lg">
-                Director
-              </Title>
-              <Text>{director?.name || "N/A"}</Text>
-            </Stack>
+        <div className={classes.imgContainer}>
+          <Container>
+            <Image
+              src={getTMDBShowcaseImageUrl(
+                isDesktop
+                  ? (media.backdrop_path as string)
+                  : (media.poster_path as string),
+                isDesktop
+              )}
+              alt={`${media.title ?? media.name} poster`}
+            />
+            <div className={classes.overlay} />
           </Container>
-        </>
+        </div>
+        <Container className={classes.mediaContainer}>
+          <Stack spacing="sm" p="1rem">
+            <Group position="apart">
+              <Title order={1}>{media.title ?? media.name}</Title>
+              <Group>
+                <AddMediaButton
+                  media={media}
+                  menuProps={{ position: "top-end" }}
+                />
+
+                {status == "authenticated" && (
+                  <Menu position="top-end">
+                    <Menu.Target>
+                      <ActionIcon variant="outline" radius="xl">
+                        <IconDotsVertical />
+                      </ActionIcon>
+                    </Menu.Target>
+                    <Menu.Dropdown className={classes.menuDropdown}>
+                      <Menu.Item
+                        icon={<IconHistory />}
+                        onClick={communityModalControls.open}
+                      >
+                        Add to watched list
+                      </Menu.Item>
+                    </Menu.Dropdown>
+                  </Menu>
+                )}
+              </Group>
+            </Group>
+            <Group spacing="sm">
+              <Badge
+                variant="outline"
+                radius="xs"
+                className={classes.certification}
+              >
+                {certification}
+              </Badge>
+              <IconPointFilled size="0.6rem" role="separator" />
+              <Text>
+                {media.release_date || media.first_air_date
+                  ? dayjs(media.release_date ?? media.first_air_date).format(
+                    "YYYY"
+                  )
+                  : "Release Date: N/A"}
+              </Text>
+              {isMovie && (
+                <>
+                  <IconPointFilled size="0.6rem" role="separator" />
+                  <Text>{formatDuration(media.runtime!)}</Text>
+                </>
+              )}
+            </Group>
+
+            <Text>{media.genres.map((g) => g.name).join(", ")}</Text>
+            <Space h="xs" />
+
+            <Title order={2} fz="lg">
+              Overview
+            </Title>
+            <Text component="p">{media.overview}</Text>
+
+            <Divider />
+
+            <Title order={2} fz="lg">
+              Cast
+            </Title>
+
+            <ul className={styles.inlineList}>
+              {media.credits.cast.map((p) => {
+                return <li key={p.id}>{p.name}</li>;
+              })}
+            </ul>
+
+            <Divider />
+
+            <Title order={2} fz="lg">
+              Director
+            </Title>
+            <Text>{director?.name || "N/A"}</Text>
+          </Stack>
+        </Container>
+
+        <Modal
+          opened={openedCommunityModal}
+          onClose={communityModalControls.close}
+          title="Commuunity"
+          centered
+        >
+          <Stack>
+            {loadingAddToList ? (
+              <Center>
+                <Loader />
+              </Center>
+            ) : (
+              communities.map((c) => (
+                <UnstyledButton
+                  className={classes.communityBtn}
+                  key={c.id}
+                  onClick={() =>
+                    addToList(media, { id: c.id, name: c.name }, true)
+                  }
+                >
+                  <Avatar size="md" radius="lg">
+                    {c.name[0].toUpperCase()}
+                  </Avatar>
+                  {c.name}
+                </UnstyledButton>
+              ))
+            )}
+          </Stack>
+        </Modal>
       </>
     </>
   );
@@ -319,9 +356,8 @@ const Media: NextPage<MediaProps> = ({ media }: MediaProps) => {
 
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const slug = query.slug;
-  const queryStr = `append_to_response=${
-    slug![0] === "movie" ? "release_dates" : "content_ratings"
-  },credits`;
+  const queryStr = `append_to_response=${slug![0] === "movie" ? "release_dates" : "content_ratings"
+    },credits`;
   const url = buildTMDBQuery(`${slug![0]}/${slug![1]}`, queryStr);
   const dataRes = await fetch(url);
   const data = await dataRes.json();
